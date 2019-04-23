a347305c14f8ce8c2141068721e3213e3a0c5336
Argenis De La Rosa <hennyrosexo@gmail.com>
2019-04-22 23:59:26 -0400

    configured stackviews into the components

--- Instagram/Controller/LoginVC.swift
+++ Instagram/Controller/LoginVC.swift
@@ -10,24 +10,58 @@ import UIKit
 
 class LoginVC: UIViewController {
     
-    //CREATING TEXT FIELD OBJECT
+    //CREATING OBJECTS
     let emailTextField: UITextField = {
         let tf = UITextField()
-        tf.placeholder = "email"
+        tf.placeholder = "Email"
         tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
         tf.borderStyle = .roundedRect
         tf.font = UIFont.systemFont(ofSize: 14)
         return tf
     }()
+    
+    let passwordTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Password"
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        tf.borderStyle = .roundedRect
+        tf.font = UIFont.systemFont(ofSize: 14)
+        return tf
+    }()
+    
+    let loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Login", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = UIColor(red: 149/255, green: 204/255, blue: 204/255, alpha: 1)
+        button.layer.cornerRadius = 5
+        return button
+    }()
 
+    // VIEW DID LOAD
     override func viewDidLoad() {
         super.viewDidLoad()
         
        //BACKGROUND COLOR
         view.backgroundColor = .white
         
-        view.addSubview(emailTextField)
-        emailTextField.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 40, paddingLeft: 20, paddingBottom: 0, paddingRight: 20, width: 0, height: 40)
+        configureViewComponents()
+    }
+    
+    //SETTING UP STACKVIEWS 
+    func configureViewComponents() {
+        
+        
+        let stackViews = UIStackView(arrangedSubviews: [emailTextField, passwordTextField, loginButton])
+        
+        stackViews.axis = .vertical
+        stackViews.spacing = 10
+        stackViews.distribution = .fillEqually
+        
+        view.addSubview(stackViews)
+        stackViews.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 40, paddingLeft: 40, paddingBottom: 0, paddingRight: 40, width: 0, height: 140)
+        
+        
     }
     
 
